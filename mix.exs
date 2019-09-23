c94c30549779801a3cf3b421c9d273d18bc9dd89
Nick Vernij <hello@nickforall.nl>
2019-09-23 15:44:01 +0200

    Prepare hex release

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule ExMessagebird.MixProject do
       version: "0.1.0-pre-release.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -23,6 +24,17 @@ defmodule ExMessagebird.MixProject do
     [
       {:httpoison, "~> 1.5"},
       {:poison, "~> 4.0", only: [:dev, :test]}
+    ]
+  end
+
+  defp package do
+    [
+      description: "An API wrapper for the Messagebird API",
+      licenses: ["MIT"],
+      maintainers: ["Nick Vernij"],
+      links: %{
+        "GitHub" => "https://github.com/Nickforall/ExMessagebird"
+      }
     ]
   end
 end
