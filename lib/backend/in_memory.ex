7437de80d4ddb352eb8f637df62ab1ab88a6e019
Nick Vernij <hello@nickforall.nl>
2019-09-30 15:19:43 +0200

    Add methods for interfacing with InMemory sevrer

--- lib/backend/in_memory.ex
+++ lib/backend/in_memory.ex
@@ -4,8 +4,18 @@ defmodule ExMessagebird.Backend.InMemory do
   """
 
   @behaviour ExMessagebird.Backend.Behaviour
+  @server ExMessagebird.Backend.InMemory.Server
 
-  def send_message(options) do
-    {:ok, options}
+  def send_message(options) when is_map(options) do
+    reply = GenServer.call(@server, {:push, options})
+    {:ok, reply}
+  end
+
+  @doc """
+  Lists all the messages sent to the InMemory store ordered by descending index
+  """
+  def list_sent_messages do
+    GenServer.call(@server, :list)
+    |> Enum.sort(fn %{index: a}, %{index: b} -> a >= b end)
   end
 end
